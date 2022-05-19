482a62ec8d314bc9e34dbd3ed5585525525f8b56
MashimaShiomi <shio.mashima@gmail.com>
2022-05-19 09:39:40 +0900

    numberカラムを追加

--- components/VueGoodTableForm.vue
+++ components/VueGoodTableForm.vue
@@ -5,9 +5,16 @@
       :rows="data"
       max-height="300px"
       :fixed-header="true"
-      :line-numbers="true"
       @on-sort-change="onSortChange"
     >
+    <template slot="table-row" slot-scope="props">
+      <span v-if="props.column.field == 'number'">
+        {{ number + props.row.originalIndex }}
+      </span>
+      <span v-else>
+        {{props.formattedRow[props.column.field]}}
+      </span>
+    </template>
     <div slot="table-actions">
       page: {{ currentPage }} / {{ totalPage }}
       <button @click="back()">back</button>
@@ -48,12 +55,17 @@ export default {
     },
     columns() {
       const labels = Object.keys(this.data[0])
-      return _.map(labels, function(value) {
+      const columnList = _.map(labels, function(value) {
         return {
           label: value,
           field: value
         }
       })
+      columnList.unshift({label: 'Number', field: 'number'});
+      return columnList
+    },
+    number() {
+      return (this.currentPage - 1) * this.per_page + 1
     }
   },
 
