b9121b4b6be9984c44c692b3fbe03887a6542d30
MashimaShiomi <shio.mashima@gmail.com>
2022-05-22 10:32:16 +0900

    エラーを解消

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue-good-table', ssr: false },
+    { src: '~/plugins/vue-good-table', ssr: true },
     { src: '~plugins/lodash.js' },
   ],
   // Auto import components: https://go.nuxtjs.dev/config-components
